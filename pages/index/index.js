26137bd1389cab6c125461649ca799bdc525eb83
zzh <185736951@qq.com>
2019-08-07 18:32:19 +0800

    v1.1 最新优化版本

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,271 +1,229 @@
 //index.js
 //获取应用实例
+import {
+  list1,
+  list2
+} from './mock.js'
+
 const app = getApp()
 let videoContext = null; //video实例
-let stopTouch = false; //滑动权限
-let deltaY = 0; //滑动距离
-let videoList = [{
-  id: '111',
-  title: '标题1',
-  like: 0,
-  likeCount: 111,
-  follow: 0,
-  topic: '帅气',
-  headPic: 'https://mp-zhuantui-1259100054.cos.ap-guangzhou.myqcloud.com/demo/my.jpg',
-  site: '深圳南山小院',
-  description: '描述1111111111',
-  src: 'https://mp-zhuantui-1259100054.cos.ap-guangzhou.myqcloud.com/videoDemo/uMt7xJfz6DUA.mp4',
-  pic: 'https://jxtt.diangoumall.com/149c30b0vodcq1258058953/0c8c9fed5285890784213263168/5285890784213263169.jpg'
-}, {
-  id: '222',
-  title: '标题1',
-  like: 0,
-  likeCount: 111,
-  follow: 0,
-  topic: '帅气',
-  headPic: 'https://mp-zhuantui-1259100054.cos.ap-guangzhou.myqcloud.com/demo/my.jpg',
-  site: '深圳南山小院',
-  description: '描述1111111111',
-  src: 'https://mp-zhuantui-1259100054.cos.ap-guangzhou.myqcloud.com/videoDemo/uMt7xJfz6DUA.mp4',
-  pic: 'https://jxtt.diangoumall.com/149c30b0vodcq1258058953/0c8c9fed5285890784213263168/5285890784213263169.jpg'
-}, {
-  id: '333',
-  title: '标题1',
-  like: 0,
-  likeCount: 111,
-  follow: 0,
-  topic: '帅气',
-  headPic: 'https://mp-zhuantui-1259100054.cos.ap-guangzhou.myqcloud.com/demo/my.jpg',
-  site: '深圳南山小院',
-  description: '描述1111111111',
-  src: 'https://mp-zhuantui-1259100054.cos.ap-guangzhou.myqcloud.com/videoDemo/uMt7xJfz6DUA.mp4',
-  pic: 'https://jxtt.diangoumall.com/149c30b0vodcq1258058953/0c8c9fed5285890784213263168/5285890784213263169.jpg'
-}, {
-  id: '444',
-  title: '标题1',
-  like: 0,
-  likeCount: 111,
-  follow: 0,
-  topic: '帅气',
-  headPic: 'https://mp-zhuantui-1259100054.cos.ap-guangzhou.myqcloud.com/demo/my.jpg',
-  site: '深圳南山小院',
-  description: '描述1111111111',
-  src: 'https://mp-zhuantui-1259100054.cos.ap-guangzhou.myqcloud.com/videoDemo/uMt7xJfz6DUA.mp4',
-  pic: 'https://jxtt.diangoumall.com/149c30b0vodcq1258058953/0c8c9fed5285890784213263168/5285890784213263169.jpg'
-}, {
-  id: '555',
-  title: '标题1',
-  like: 0,
-  likeCount: 111,
-  follow: 0,
-  topic: '帅气',
-  headPic: 'https://mp-zhuantui-1259100054.cos.ap-guangzhou.myqcloud.com/demo/my.jpg',
-  site: '深圳南山小院',
-  description: '描述1111111111',
-  src: 'https://mp-zhuantui-1259100054.cos.ap-guangzhou.myqcloud.com/videoDemo/uMt7xJfz6DUA.mp4',
-  pic: 'https://jxtt.diangoumall.com/149c30b0vodcq1258058953/0c8c9fed5285890784213263168/5285890784213263169.jpg'
-}]
+let time=null
+
 Page({
   data: {
+    // 视频Id
+    videoId: 1,
+    // 当前swiper所在的位置
+    current: 0,
     // 视频列表
-    videoList,
+    videoList: [],
     // 当前播放视频
-    videoParam: {
-      id: '333',
-      title: '标题3',
-      like: 0,
-      likeCount: 333,
-      follow: 0,
-      topic: '帅气',
-      headPic: 'https://mp-zhuantui-1259100054.cos.ap-guangzhou.myqcloud.com/demo/my.jpg',
-      site: '深圳南山小院',
-      description: '描述33333333333',
-      src: 'https://mp-zhuantui-1259100054.cos.ap-guangzhou.myqcloud.com/videoDemo/uMt7xJfz6DUA.mp4'
-    },
-    //滑动参数
-    touch: {
-      startX: 0,
-      startY: 0,
-      time: 0
-    },
-    videoId: '333', // 视频Id
-    videoInde: '', //视频索引
-    translateParam: "translateY(-100%)", //视图位置
-    progressTime: "0%", //进度条
+    videoParam: {},
+    // 播放开关 1是暂停 2是播放
+    playMark: 2,
+
+    // 第一个item
+    oneItemParam: {},
+    // 第二个item
+    twoItemParam: {},
+    // 第一个item
+    threeItemParam: {},
   },
   onLoad: function(e) {
+    // 拿到当前视频的实例
+    this.videoContext0 = wx.createVideoContext('myVideo0')
+    // 拿到当前视频的实例
+    this.videoContext1 = wx.createVideoContext('myVideo1')
+    // 拿到当前视频的实例
+    this.videoContext2 = wx.createVideoContext('myVideo2')
+
+    /* 初始化页面视频id 及 视频下标 */
     this.setData({
-      videoId: e.videoId || "444"
+      videoId: e.videoId || 1,
+      videoList: list1
     })
 
     let videoIndex = this.data.videoList.findIndex(v => v.id == this.data.videoId)
-    this.setData({
-      videoIndex
-    })
+    let current = videoIndex % 3
 
-    this.tabHome(videoIndex)
-    this.watchVideoId()
-  },
-  // 模拟vue watch 待学习 太懒 后面补充 
-  watchVideoId() {
-    let list = this.data.videoList
-    let videoIndex = this.data.videoIndex
-    this.setData({
-      videoParam: list[videoIndex]
-    })
+    /* 拿到对应视频的数据 */
+    this.getVideoParam(videoIndex)
 
-    let l = list.length - 1
-    // 当滑动到最后一个视频的时候 加载视频列表
-    if (l == videoIndex) {
-      console.log('滑动到最后一个是该加载数据啦')
-      let list = this.data.videoList
+    if (current === 0) {
       this.setData({
-        videoList: [...list, ...videoList]
+        oneItemParam: list1[videoIndex],
       })
     }
 
-    this.setVideoParam(videoIndex)
-  },
-  // 修改视频数据
-  setVideoParam(i) {
-    this.setData({
-      videoParam: this.data.videoList[i], //数据初始化
-      progressTime: "0%", //进度条初始化
-    })
-
-    //异步播放 避免获取不到video实例
-    videoContext = wx.createVideoContext('myVideo' + this.data.videoIndex)
-    videoContext && videoContext.play()
-  },
-  // 切换视频
-  tabHome(i) {
-    let top = -(i) * 100
-    this.setData({
-      translateParam: `translateY(${top}%)`
-    })
-  },
-  // 播放心跳
-  timeupdate(e) {
-    // console.log(e)
-    // let total = e.detail.duration || 0
-    // let newTime = e.detail.currentTime || 0
-    // this.setData({
-    //   progressTime: (newTime / total * 100) + "%" || this.data.progressTime
-    // })
-  },
-  onTouchstart(e) {
-    // console.log('开始')
-    let touch = e.touches[0];
-    this.data.touch.startX = touch.pageX;
-    this.data.touch.startY = touch.pageY;
-    this.data.touch.time = new Date().getTime();
-  },
-  onTouchmove(e) {
-    // console.log('过程')
-    let touch = e.touches[0];
-    let deltaX = touch.pageX - this.data.touch.startX;
-    deltaY = touch.pageY - this.data.touch.startY;
-    if (Math.abs(deltaX) > Math.abs(deltaY)) {
-      // console.log('左右不管')
-
+    if (current === 1) {
+      this.setData({
+        twoItemParam: list1[videoIndex],
+      })
     }
 
-    if (deltaY > 0) {
-      // console.log('我是下滑')
+    if (current === 2) {
+      this.setData({
+        threeItemParam: list1[videoIndex],
+      })
+    }
 
+    // 匹配对应数据
+    this.tabItem(current, videoIndex)
+
+    /* 开始播放视频 */
+    this.play(videoIndex)
+  },
+  changeCurrent(e){
+    // 当前视频的数据列表
+    let videoList = this.data.videoList
+
+    // 当前视图视频id
+    let id = e.detail.currentItemId
+
+    /* 根据当前视图视频id拿到对应的视频列表下标 */
+    let videoIndex = videoList.findIndex(v => v.id == id)
+
+    /* 滑动到倒数第二个视频 加载视频列表 */
+    if (videoIndex == (videoList.length - 2)) {
+      let newList = [...this.data.videoList, ...list2]
+      this.setData({
+        videoList: newList
+      })
+    }
+  },
+  /**
+   *  current 变化时 修改视频容器对应的数据
+   */
+  changeItem(e) {
+    // 当前item的位置
+    let current = this.data.current
+
+    // swiper滑到的位置
+    let swiperIndex = e.detail.current
+
+    // 如果没有切换就不执行其他操作
+    if (current == swiperIndex)return;
+
+    // 当前视频的数据列表
+    let videoList = this.data.videoList
+
+    // 当前视图视频id
+    let id = e.detail.currentItemId
+
+    /* 根据当前视图视频id拿到对应的视频列表下标 */
+    let videoIndex = videoList.findIndex(v => v.id == id)
+
+    /* 拿到对应视频的数据 */
+    this.getVideoParam(videoIndex)
+
+    // 匹配对应数据
+    this.tabItem(swiperIndex, videoIndex)
+
+    /* 销毁视频实例 */
+    this.stop()
+    
+    /* 开始播放视频 */
+    this.play(swiperIndex)
+  },
+  // 根据swiperIndex videoIndex匹配对应数据
+  tabItem(swiperIndex, videoIndex) {
+    // 当前视频的数据列表
+    let videoList = JSON.parse(JSON.stringify(this.data.videoList))
+
+    let next = 0
+    let first = 0
+
+    /* 上一个 */
+    if (videoIndex == 0) {
+      first = videoList.length - 1
     } else {
-      // console.log('我是上滑')
-
-    }
-  },
-  onTouchend(e) {
-    // console.log('结束')
-    // 阻止连续滑动
-    if (stopTouch) return;
-
-    let startTime = this.data.touch.time //start时间
-    let endTime = new Date().getTime() //end时间
-    let list = this.data.videoList //视频数组
-    let videoIndex = this.data.videoIndex //视频索引
-    let videoId = this.data.videoId //视频id
-    console.log(videoIndex)
-    //时间超过3秒滑动无效 这里暂时不操作
-    if ((endTime - startTime) > 3000) {
-
+      first = videoIndex - 1
     }
 
-    // 不能连续滑动 必须等待滑动完成动画过渡
-    if (deltaY > 30 || deltaY < -30) {
-      this.stoptouchFunc()
+    /* 下一个 */
+    if (videoIndex == videoList.length - 1) {
+      next = 0
+    } else {
+      next = videoIndex + 1
     }
 
-    // 滑动距离不够
-    if (deltaY < 30 && deltaY > -30) {
-      return
+    switch (swiperIndex) {
+      case 0:
+        this.setData({
+          twoItemParam: videoList[next],
+          threeItemParam: videoList[first]
+        })
+        break;
+      case 1:
+        this.setData({
+          oneItemParam: videoList[first],
+          threeItemParam: videoList[next]
+        })
+        break;
+      case 2:
+        this.setData({
+          oneItemParam: videoList[next],
+          twoItemParam: videoList[first],
+        })
+        break;
+      default:
     }
 
-    // 滑动超过30执行 下滑
-    if (deltaY > 30) {
-      console.log('下滑可以执行切换')
-      // 到第一个视频的时候不能在继续滑动了
-      if ((videoIndex == 0)) {
-        console.log('到头了 不能在往上了')
-        return
-      }
-
-      videoIndex -= 1
-      this.tabHome(videoIndex)
-    }
-
-    // 滑动小于-30执行 上滑
-    if (deltaY < -30) {
-      console.log('上滑可以执行切换')
-      // 到第一个视频的时候不能在继续滑动了 或者需要加载数据
-      if ((videoIndex == list.length - 1)) {
-        console.log('到底了 不能在往下了')
-        return
-      }
-
-      videoIndex += 1
-      this.tabHome(videoIndex)
-    }
-
-    console.log(videoIndex)
-    // 停止视频
-    this.deteleVideo()
-
-    //初始化配置参数
-    deltaY = 0
     this.setData({
-      ['touch.time']: 0
-    })
-
-    setTimeout(() => {
-      // 先执行动画 在切换视频 提升流畅性
-      this.setIndex(videoIndex)
-      this.watchVideoId()
-    }, 400)
-  },
-  // 根据索引修改视频id
-  setIndex(i) {
-    this.setData({
-      videoIndex: i
+      current: swiperIndex
     })
   },
-  //暂停或停止video
-  deteleVideo() {
-    videoContext = wx.createVideoContext('myVideo' + this.data.videoIndex)
-    videoContext && videoContext.pause()
-    videoContext && videoContext.stop()
+  // 拿到当前视图的视频数据
+  getVideoParam(videoIndex) {
+    // 当前视频的数据列表
+    let videoList = JSON.parse(JSON.stringify(this.data.videoList))
+    this.setData({
+      videoParam: videoList[videoIndex]
+    })
   },
-  stoptouchFunc() {
-    // 锁定滑动权限
-    stopTouch = true
-
-    // 动画是0.4s 预估有一点dom渲染时间 0.2s 就延迟0.7s 时间在准滑动哈
-    setTimeout(() => {
-      stopTouch = false
-    }, 700)
+  // 播放
+  play(swiperIndex) {
+    let index = parseInt(swiperIndex)
+    switch (index) {
+      case 0:
+        this.videoContext0 && this.videoContext0.play()
+        break;
+      case 1:
+        this.videoContext1 && this.videoContext1.play()
+        break;
+      case 2:
+        this.videoContext2 && this.videoContext2.play()
+        break;
+      default:
+    }
+  },
+  // 销毁视频 注意 必须在切换之前销毁
+  stop() {
+    this.videoContext0 && this.videoContext0.pause()
+    this.videoContext1 && this.videoContext1.pause()
+    this.videoContext2 && this.videoContext2.pause()
+  },
+  // 开始播放 
+  eventPlay(e) {
+    this.setData({
+      playMark: 2
+    })
+  },
+  // 点击视频
+  tabVideo(e) {
+    let swiperIndex = this.data.current
+    let playMark = this.data.playMark
+    if (playMark == 2) {
+      this.stop()
+      this.setData({
+        playMark: 1
+      })
+    } else {
+      this.play(swiperIndex)
+      this.setData({
+        playMark: 2
+      })
+    }
   },
   // 点赞
   videoLike() {
@@ -276,5 +234,5 @@ Page({
       ['videoParam.like']: videoParam.like == 1 ? 0 : 1,
       ['videoParam.likeCount']: videoParam.like == 1 ? videoParam.likeCount - 1 : videoParam.likeCount + 1
     })
-  }
+  },
 })
---
+++ pages/index/mock.js
@@ -0,0 +1,164 @@
+export const list1 = [
+  {
+    id: 1,
+    title: '标题1',
+    like: 0,
+    likeCount: 111,
+    follow: 0,
+    topic: '帅气',
+    headPic: 'https://mp-zhuantui-1259100054.cos.ap-guangzhou.myqcloud.com/demo/my.jpg',
+    site: '深圳南山小院',
+    description: '描述1111111111',
+    src: 'https://jxtt.diangoumall.com/149c30b0vodcq1258058953/49c7db9a5285890787353753574/Q5UBGGmat3IA.mp4',
+    pic: 'http://jxtt.diangoumall.com/149c30b0vodcq1258058953/d40981735285890792272813628/5285890792272813629.png'
+  },
+  {
+    id: 2,
+    title: '标题1',
+    like: 0,
+    likeCount: 111,
+    follow: 0,
+    topic: '帅气',
+    headPic: 'https://mp-zhuantui-1259100054.cos.ap-guangzhou.myqcloud.com/demo/my.jpg',
+    site: '深圳南山小院',
+    description: '描述22222222222',
+    src: 'http://jxtt.diangoumall.com/149c30b0vodcq1258058953/64f5e9d45285890792488081407/8oURskeCNB4A.mp4',
+    pic: 'http://jxtt.diangoumall.com/149c30b0vodcq1258058953/8f50418c5285890792278024360/5285890792278024361.jpg'
+  },
+  {
+    id: 3,
+    title: '标题1',
+    like: 0,
+    likeCount: 111,
+    follow: 0,
+    topic: '帅气',
+    headPic: 'https://mp-zhuantui-1259100054.cos.ap-guangzhou.myqcloud.com/demo/my.jpg',
+    site: '深圳南山小院',
+    description: '描述33333333333',
+    src: 'http://jxtt.diangoumall.com/149c30b0vodcq1258058953/d40981735285890792272813628/0WwtSZ6LaQsA.mp4',
+    pic: 'http://jxtt.diangoumall.com/149c30b0vodcq1258058953/028c7b2e5285890790259303635/5285890790259303636.jpg'
+  },
+  {
+    id: 4,
+    title: '标题1',
+    like: 0,
+    likeCount: 111,
+    follow: 0,
+    topic: '帅气',
+    headPic: 'https://mp-zhuantui-1259100054.cos.ap-guangzhou.myqcloud.com/demo/my.jpg',
+    site: '深圳南山小院',
+    description: '描述44444444444',
+    src: 'http://jxtt.diangoumall.com/149c30b0vodcq1258058953/8f50418c5285890792278024360/culTK7B9wQUA.mp4',
+    pic: 'http://jxtt.diangoumall.com/149c30b0vodcq1258058953/f242fcb05285890790418518803/5285890790418518804.jpg'
+  },
+  {
+    id: 5,
+    title: '标题1',
+    like: 0,
+    likeCount: 111,
+    follow: 0,
+    topic: '帅气',
+    headPic: 'https://mp-zhuantui-1259100054.cos.ap-guangzhou.myqcloud.com/demo/my.jpg',
+    site: '深圳南山小院',
+    description: '描述5555555555555',
+    src: 'http://jxtt.diangoumall.com/149c30b0vodcq1258058953/028c7b2e5285890790259303635/xakDxdIyH40A.mp4',
+    pic: 'http://jxtt.diangoumall.com/149c30b0vodcq1258058953/23b7ad255285890792120624145/5285890792120624146.jpg'
+  },
+  {
+    id: 6,
+    title: '标题1',
+    like: 0,
+    likeCount: 111,
+    follow: 0,
+    topic: '帅气',
+    headPic: 'https://mp-zhuantui-1259100054.cos.ap-guangzhou.myqcloud.com/demo/my.jpg',
+    site: '深圳南山小院',
+    description: '描述666666666666666',
+    src: 'http://jxtt.diangoumall.com/149c30b0vodcq1258058953/f242fcb05285890790418518803/pyuSPahvZJYA.mp4',
+    pic: 'http://jxtt.diangoumall.com/149c30b0vodcq1258058953/b5cf86cd5285890791051309493/5285890791051309494.jpg'
+  },
+]
+
+
+export const list2 = [
+  {
+    id: 7,
+    title: '标题1',
+    like: 0,
+    likeCount: 111,
+    follow: 0,
+    topic: '帅气',
+    headPic: 'https://mp-zhuantui-1259100054.cos.ap-guangzhou.myqcloud.com/demo/my.jpg',
+    site: '深圳南山小院',
+    description: '描述777777777',
+    src: 'http://jxtt.diangoumall.com/149c30b0vodcq1258058953/23b7ad255285890792120624145/qvGzDAESscgA.mp4',
+    pic: 'http://jxtt.diangoumall.com/149c30b0vodcq1258058953/add3fcfa5285890792400897279/5285890792400897280.jpg'
+  },
+  {
+    id: 8,
+    title: '标题1',
+    like: 0,
+    likeCount: 111,
+    follow: 0,
+    topic: '帅气',
+    headPic: 'https://mp-zhuantui-1259100054.cos.ap-guangzhou.myqcloud.com/demo/my.jpg',
+    site: '深圳南山小院',
+    description: '描述88888888',
+    src: 'http://jxtt.diangoumall.com/149c30b0vodcq1258058953/b5cf86cd5285890791051309493/NQYuQR6PHUUA.mp4',
+    pic: 'http://jxtt.diangoumall.com/149c30b0vodcq1258058953/571e296f5285890792433782008/5285890792433782010.jpg'
+  },
+  {
+    id: 9,
+    title: '标题1',
+    like: 0,
+    likeCount: 111,
+    follow: 0,
+    topic: '帅气',
+    headPic: 'https://mp-zhuantui-1259100054.cos.ap-guangzhou.myqcloud.com/demo/my.jpg',
+    site: '深圳南山小院',
+    description: '描述99999999',
+    src: 'http://jxtt.diangoumall.com/149c30b0vodcq1258058953/add3fcfa5285890792400897279/J68IWjyhVt0A.mp4',
+    pic: 'http://jxtt.diangoumall.com/149c30b0vodcq1258058953/b37367f55285890790258233848/5285890790258233849.jpg'
+  },
+  {
+    id: 10,
+    title: '标题1',
+    like: 0,
+    likeCount: 111,
+    follow: 0,
+    topic: '帅气',
+    headPic: 'https://mp-zhuantui-1259100054.cos.ap-guangzhou.myqcloud.com/demo/my.jpg',
+    site: '深圳南山小院',
+    description: '描述1010101010',
+    src: 'http://jxtt.diangoumall.com/149c30b0vodcq1258058953/571e296f5285890792433782008/RjqYUPUdKskA.mp4',
+    pic: 'http://jxtt.diangoumall.com/149c30b0vodcq1258058953/1304a2995285890792449807508/5285890792449807509.jpg'
+  },
+  {
+    id: 11,
+    title: '标题1',
+    like: 0,
+    likeCount: 111,
+    follow: 0,
+    topic: '帅气',
+    headPic: 'https://mp-zhuantui-1259100054.cos.ap-guangzhou.myqcloud.com/demo/my.jpg',
+    site: '深圳南山小院',
+    description: '描述111111111111111111111111',
+    src: 'http://jxtt.diangoumall.com/149c30b0vodcq1258058953/b37367f55285890790258233848/DY6aaTzbJKcA.mp4',
+    pic: 'http://jxtt.diangoumall.com/149c30b0vodcq1258058953/7c9f7a335285890792437129391/5285890792437129392.jpg'
+  },
+  {
+    id: 12,
+    title: '标题1',
+    like: 0,
+    likeCount: 111,
+    follow: 0,
+    topic: '帅气',
+    headPic: 'https://mp-zhuantui-1259100054.cos.ap-guangzhou.myqcloud.com/demo/my.jpg',
+    site: '深圳南山小院',
+    description: '描述121212121212121212121212',
+    src: 'http://jxtt.diangoumall.com/149c30b0vodcq1258058953/1304a2995285890792449807508/0Afh78SL0jQA.mp4',
+    pic: 'http://jxtt.diangoumall.com/149c30b0vodcq1258058953/7c9f7a335285890792437129391/5285890792437129392.jpg'
+  },
+]
+
+
