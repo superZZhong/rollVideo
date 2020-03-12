ae3ee588258bd54e912e0f854bf9e93280122468
zzh <185736951@qq.com>
2020-03-12 17:51:48 +0800

    最新优化，把逻辑视频列表数除3取余，余数相加在除直到最后一页没有数据的情况下展示

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,238 +1,292 @@
-//index.js
-//获取应用实例
-import {
-  list1,
-  list2
-} from './mock.js'
-
+import { defaultVideo1, defaultVideo2} from "./mock/videoList.js"
 const app = getApp()
-let videoContext = null; //video实例
-let time=null
 
 Page({
   data: {
-    // 视频Id
-    videoId: 1,
-    // 当前swiper所在的位置
+    circular: false,
+    // 当前播放视频的数据
+    videoInfo: {
+      index: 0,
+    },
+    // 滑块
     current: 0,
-    // 视频列表
-    videoList: [],
-    // 当前播放视频
-    videoParam: {},
-    // 播放开关 1是暂停 2是播放
-    playMark: 2,
-
-    // 第一个item
-    oneItemParam: {},
-    // 第二个item
-    twoItemParam: {},
-    // 第一个item
-    threeItemParam: {},
-  },
-  onLoad: function(e) {
-    // 拿到当前视频的实例
-    this.videoContext0 = wx.createVideoContext('myVideo0')
-    // 拿到当前视频的实例
-    this.videoContext1 = wx.createVideoContext('myVideo1')
-    // 拿到当前视频的实例
-    this.videoContext2 = wx.createVideoContext('myVideo2')
-
-    /* 初始化页面视频id 及 视频下标 */
-    this.setData({
-      videoId: e.videoId || 1,
-      videoList: list1
-    })
-
-    let videoIndex = this.data.videoList.findIndex(v => v.id == this.data.videoId)
-    let current = videoIndex % 3
-
-    /* 拿到对应视频的数据 */
-    this.getVideoParam(videoIndex)
-
-    if (current === 0) {
-      this.setData({
-        oneItemParam: list1[videoIndex],
-      })
-    }
-
-    if (current === 1) {
-      this.setData({
-        twoItemParam: list1[videoIndex],
-      })
-    }
-
-    if (current === 2) {
-      this.setData({
-        threeItemParam: list1[videoIndex],
-      })
-    }
-
-    // 匹配对应数据
-    this.tabItem(current, videoIndex)
-
-    /* 开始播放视频 */
-    this.play(videoIndex)
-  },
-  changeCurrent(e){
-    // 当前视频的数据列表
-    let videoList = this.data.videoList
-
-    // 当前视图视频id
-    let id = e.detail.currentItemId
-
-    /* 根据当前视图视频id拿到对应的视频列表下标 */
-    let videoIndex = videoList.findIndex(v => v.id == id)
-
-    /* 滑动到倒数第二个视频 加载视频列表 */
-    if (videoIndex == (videoList.length - 2)) {
-      let newList = [...this.data.videoList, ...list2]
-      this.setData({
-        videoList: newList
-      })
+    // 播放状态
+    playStatus: false,
+    videoObjList: {
+      0: {},
+      1: {},
+      2: {},
+    },
+    paging: {
+      page: 1,
+      size: 9
     }
   },
-  /**
-   *  current 变化时 修改视频容器对应的数据
-   */
-  changeItem(e) {
-    // 当前item的位置
-    let current = this.data.current
+  onShow() {
 
-    // swiper滑到的位置
-    let swiperIndex = e.detail.current
-
-    // 如果没有切换就不执行其他操作
-    if (current == swiperIndex)return;
-
-    // 当前视频的数据列表
-    let videoList = this.data.videoList
-
-    // 当前视图视频id
-    let id = e.detail.currentItemId
-
-    /* 根据当前视图视频id拿到对应的视频列表下标 */
-    let videoIndex = videoList.findIndex(v => v.id == id)
-
-    /* 拿到对应视频的数据 */
-    this.getVideoParam(videoIndex)
-
-    // 匹配对应数据
-    this.tabItem(swiperIndex, videoIndex)
-
-    /* 销毁视频实例 */
-    this.stop()
-    
-    /* 开始播放视频 */
-    this.play(swiperIndex)
   },
-  // 根据swiperIndex videoIndex匹配对应数据
-  tabItem(swiperIndex, videoIndex) {
-    // 当前视频的数据列表
-    let videoList = JSON.parse(JSON.stringify(this.data.videoList))
+  onLoad(e) {
+    this.pageStatus = 0 // 页面初始化状态
+    this.videoKey = "video" // 视频id key
+    this.videoList = [] // 渲染的视频
+    this.residueVideoList = [] // 取余的数组
+    this.videoIndex = 0 // 当前视频的位置
+    this.loadingVideo = 0
 
-    let next = 0
-    let first = 0
+  },
+  onReady() {
+    this.videoObj0 = this.getVideoObj(this.videoKey, 0)
+    this.videoObj1 = this.getVideoObj(this.videoKey, 1)
+    this.videoObj2 = this.getVideoObj(this.videoKey, 2)
 
-    /* 上一个 */
+    // 获取视频列表
+    this.getVideoList()
+  },
+  // 获取视频列表
+  getVideoList() {
+    setTimeout(() => {
+      let oldVideoLength = this.videoList.length
+
+      // 这段模拟异步数据的获取
+      let pageList = []
+      if (this.data.paging.page == 1) {
+        pageList = defaultVideo1
+      } else if (this.data.paging.page == 2) {
+        pageList = defaultVideo2
+      } else {
+        pageList = []
+      }
+      console.log(this.data.paging.page)
+      console.log(pageList)
+      console.log(this.residueVideoList)
+
+      pageList = pageList.map(v => {
+        // v.videoUrl = ase.decrypt(v.videoUrl || "") // 视频链接加密 防止链接被盗用
+
+        return v
+      })
+
+      // 无数据的情况
+      if (pageList.length < 1 && this.data.paging.page == 1) {
+        return
+      }
+
+      // 如果数据少于1重新拿分页为1的数据
+      if (pageList.length < 1 && this.data.paging.page != 1) {
+        this.setData({
+          ['paging.page']: 1,
+        })
+        this.getVideoList()
+        return
+      }
+
+      // 滑块数量
+      let itemNumber = Object.keys(this.data.videoObjList).length || 3
+
+      // 重新计算数组的排列
+      const videoData = this.formArrayVideo(this.videoList, this.residueVideoList, pageList, itemNumber)
+
+      // 渲染的数组
+      this.videoList = videoData.list.map((v, index) => {
+        return {
+          ...v,
+          index
+        }
+      })
+
+      // 取余的数组
+      this.residueVideoList = videoData.residue
+
+      // 避免去重后数据保持不变产生死循环
+      if (oldVideoLength != this.videoList.length) {
+        this.setData({
+          circular: true,
+        })
+
+        // 开始渲染
+        this.lazyVideo()
+      }
+
+      // 第一次播放
+      if (this.pageStatus == 0) {
+
+        // 第一次初始化加载完毕
+        this.pageStatus = 1
+
+        // 滑块位置
+        const current = this.data.current
+
+        // 开始播放
+        this.currentPlay(current, current)
+
+      }
+
+      this.setData({
+        ['paging.page']: this.data.paging.page + 1,
+      })
+
+    }, 300)
+  },
+  // swiper切换
+  swiperChange(e) {
+    if (e.detail.source == "touch") {
+      // 旧的滑块位置
+      const oldCurrent = this.data.current
+      // 当前滑块的位置
+      const current = e.detail.current
+      if (oldCurrent == current) return;
+
+      this.setData({
+        current
+      })
+
+      // 当前视频在列表中的索引
+      this.videoIndex = parseInt(e.detail.currentItemId)
+      console.log(this.videoIndex)
+
+      // 开始渲染
+      this.lazyVideo()
+
+      // 开始播放
+      this.currentPlay(oldCurrent, current)
+    }
+  },
+  // 加载视频
+  lazyVideo() {
+
+    const videoList = this.videoList // 渲染的数组
+    const videoIndex = this.videoIndex // 当前播放的视频在数组里的索引
+    const current = this.data.current // 滑块位置
+
+    const lastVideo = videoList[videoIndex - 1] || {}
+    const lastTwoVideo = videoList[videoIndex - 2] || {}
+    const nowVideo = videoList[videoIndex] || {}
+    const nextVideo = videoList[videoIndex + 1] || {}
+    const nextTwoVideo = videoList[videoIndex + 2] || {}
+
     if (videoIndex == 0) {
-      first = videoList.length - 1
-    } else {
-      first = videoIndex - 1
-    }
-
-    /* 下一个 */
-    if (videoIndex == videoList.length - 1) {
-      next = 0
-    } else {
-      next = videoIndex + 1
-    }
-
-    switch (swiperIndex) {
-      case 0:
-        this.setData({
-          twoItemParam: videoList[next],
-          threeItemParam: videoList[first]
-        })
-        break;
-      case 1:
-        this.setData({
-          oneItemParam: videoList[first],
-          threeItemParam: videoList[next]
-        })
-        break;
-      case 2:
-        this.setData({
-          oneItemParam: videoList[next],
-          twoItemParam: videoList[first],
-        })
-        break;
-      default:
-    }
-
-    this.setData({
-      current: swiperIndex
-    })
-  },
-  // 拿到当前视图的视频数据
-  getVideoParam(videoIndex) {
-    // 当前视频的数据列表
-    let videoList = JSON.parse(JSON.stringify(this.data.videoList))
-    this.setData({
-      videoParam: videoList[videoIndex]
-    })
-  },
-  // 播放
-  play(swiperIndex) {
-    let index = parseInt(swiperIndex)
-    switch (index) {
-      case 0:
-        this.videoContext0 && this.videoContext0.play()
-        break;
-      case 1:
-        this.videoContext1 && this.videoContext1.play()
-        break;
-      case 2:
-        this.videoContext2 && this.videoContext2.play()
-        break;
-      default:
-    }
-  },
-  // 销毁视频 注意 必须在切换之前销毁
-  stop() {
-    this.videoContext0 && this.videoContext0.pause()
-    this.videoContext1 && this.videoContext1.pause()
-    this.videoContext2 && this.videoContext2.pause()
-  },
-  // 开始播放 
-  eventPlay(e) {
-    this.setData({
-      playMark: 2
-    })
-  },
-  // 点击视频
-  tabVideo(e) {
-    let swiperIndex = this.data.current
-    let playMark = this.data.playMark
-    if (playMark == 2) {
-      this.stop()
       this.setData({
-        playMark: 1
+        circular: false,
+        ['videoObjList.0']: nowVideo,
+        ['videoObjList.1']: nextVideo,
+        ['videoObjList.2']: nextTwoVideo,
       })
-    } else {
-      this.play(swiperIndex)
+    } else if (videoIndex == videoList.length - 1) {
+      console.log('滑动到了最后一页')
       this.setData({
-        playMark: 2
+        circular: false,
+        ['videoObjList.0']: lastTwoVideo,
+        ['videoObjList.1']: lastVideo,
+        ['videoObjList.2']: nowVideo,
       })
+      // 获取视频列表
+      this.getVideoList()
+    } else {
+      if (current == 0) {
+        this.setData({
+          circular: true,
+          ['videoObjList.0']: nowVideo,
+          ['videoObjList.1']: nextVideo,
+          ['videoObjList.2']: lastVideo,
+        })
+      }
+      if (current == 1) {
+        this.setData({
+          circular: true,
+          ['videoObjList.0']: lastVideo,
+          ['videoObjList.1']: nowVideo,
+          ['videoObjList.2']: nextVideo,
+        })
+      }
+      if (current == 2) {
+        this.setData({
+          circular: true,
+          ['videoObjList.0']: nextVideo,
+          ['videoObjList.1']: lastVideo,
+          ['videoObjList.2']: nowVideo,
+        })
+      }
     }
-  },
-  // 点赞
-  videoLike() {
-    // 深拷贝
-    let videoParam = JSON.parse(JSON.stringify(this.data.videoParam))
 
     this.setData({
-      ['videoParam.like']: videoParam.like == 1 ? 0 : 1,
-      ['videoParam.likeCount']: videoParam.like == 1 ? videoParam.likeCount - 1 : videoParam.likeCount + 1
+      videoInfo: nowVideo
     })
   },
+  // 播放视频的逻辑
+  currentPlay(oldCurrent, current) {
+    // 获取上一个视频实例
+    const oldVideoObj = this.getVideoObj(this.videoKey, oldCurrent)
+
+    // 暂停上一个视频实例
+    this.pauseVideo(oldVideoObj)
+
+    // 获取当前视频实例
+    const videoObj = this.getVideoObj(this.videoKey, current)
+
+    // 播放当前视频实例
+    this.playVideo(videoObj)
+  },
+  // 播放/或暂停视频
+  tabVideo() {
+    let playStatus = this.data.playStatus
+    let current = this.data.current
+    // 获取当前视频实例
+    const videoObj = this.getVideoObj(this.videoKey, current)
+
+    if (!playStatus) {
+      // 播放停视频实例
+      this.pauseVideo(videoObj)
+    } else {
+      // 播放当前视频实例
+      this.playVideo(videoObj)
+    }
+    this.setData({
+      playStatus: !playStatus
+    })
+  },
+  // 视频开始播放
+  eventPlay() {
+    this.setData({
+      playStatus: false
+    })
+  },
+  // 播放视频
+  playVideo(videoObj) {
+    videoObj && videoObj.play()
+  },
+  // 暂停视频
+  pauseVideo(videoObj) {
+    videoObj && videoObj.pause()
+  },
+  // 销毁实例
+  deleteVideoObJ(videoObj) {
+    videoObj && videoObj.stop()
+  },
+  // 获取视频实例
+  getVideoObj(key, index) {
+    return wx.createVideoContext(key + index)
+  },
+  // 格式化数组取渲染数组及余数组
+  formArrayVideo(oldList = [], residueList = [], newlist = [], itemNumber = 3) {
+    // 原数组
+    let onwList = oldList.concat(residueList, newlist)
+    // 倍数
+    const ints = Math.floor(onwList.length / itemNumber)
+    // 最新数组
+    const list = onwList.slice(0, ints * itemNumber) || []
+    // 缓存余下的数组
+    const residue = onwList.slice(ints * itemNumber, onwList.length) || []
+
+    if (list.length < 1) {
+      return {
+        list: onwList,
+        residue: [],
+      }
+    } else {
+      return {
+        list,
+        residue,
+      }
+    }
+  }
 })
